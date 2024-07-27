eedb7c641d9317927231b0ac9ed0055e2df2689a
Nick Main <david.nick.main@gmail.com>
2024-07-27 14:53:43 -0700

    Initial commit

---
+++ quartz/quartz/components/Footer.tsx
@@ -0,0 +1,28 @@
+import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
+import style from "./styles/footer.scss"
+import { version } from "../../package.json"
+import { i18n } from "../i18n"
+
+interface Options {
+  links: Record<string, string>
+}
+
+export default ((opts?: Options) => {
+  const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
+    const year = new Date().getFullYear()
+    const links = opts?.links ?? []
+    return (
+      <footer class={`${displayClass ?? ""}`}>
+        <hr />
+         <p>© {year} David N Main. See the&nbsp;
+           <a href="https://hello.nickmain.com/r7rs/License" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">
+           license page
+           </a>.
+         </p>
+      </footer>
+    )
+  }
+
+  Footer.css = style
+  return Footer
+}) satisfies QuartzComponentConstructor
---
+++ vault/index.md
@@ -0,0 +1,27 @@
+---
+title: Site Template
+aliases:
+  - Home
+---
+
+This is a blank Quartz installation.
+See the [documentation](https://quartz.jzhao.xyz) for how to get started.
+
+#### Set up steps
+
+* #todo  Edit `quartz/quartz.config.ts` to change title and base url:
+
+```typescript
+const config: QuartzConfig = {
+  configuration: {
+    pageTitle: "Site Template",  <---- title
+    enableSPA: true,
+    enablePopovers: true,
+    analytics: null,
+    locale: "en-US",
+    baseUrl: "hello.nickmain.com/obsidian-quartz-template", <---- base url
+```
+
+* #todo  Edit `title` property of this page.
+* #todo  Edit Footer.tsx to change site URL
+* #todo  Enable Github Pages via Actions on Github web.
